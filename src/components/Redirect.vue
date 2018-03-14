8a714d8745b9cfd90d310487be1c8992d25951ac
weswu <836790625@qq.com>
2018-03-14 15:20:33 +0800

    wxw

--- src/components/Redirect.vue
+++ src/components/Redirect.vue
@@ -8,21 +8,19 @@
 export default {
   mounted () {
     var addBind = this.$route.query.addBind
-    var bindType = this.$route.query.bindType
     if (addBind && addBind === '1') {
-      console.log('QQbid')
       window.parent.postMessage({ type: 'bind' }, '*')
     } else {
-      console.log('QQclose')
       window.parent.postMessage({ type: 1 }, '*')
     }
-    if(window.opener){//判断是否有父窗口,即打开本页面的窗口
+    // 判断是否有父窗口,即打开本页面的窗口
+    if (window.opener) {
       if (addBind && addBind === '1') {
         window.opener.postMessage({type: 'bind'}, '*')
       } else {
         window.opener.postMessage({type: 1}, '*')
       }
-      window.close();  //关闭窗口
+      window.close()  // 关闭窗口
     }
   }
 
